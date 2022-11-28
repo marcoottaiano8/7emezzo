88d3c2a0121fa72a57936e240e3d2b71e9106f0a
marcoottaiano <marco.ottaiano@beije.it>
2022-11-28 16:28:37 +0100

    updating versione

--- 7emezzo-mobile/App.js
+++ 7emezzo-mobile/App.js
@@ -1,11 +1,12 @@
-import { Login } from '7emezzo-library';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Login } from "7emezzo-library";
+import Signup from "7emezzo-library/dist/screens/Signup";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View>
-      <Login />
+      <Signup />
     </View>
   );
 }
