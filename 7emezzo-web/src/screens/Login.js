78e4899be24449bac57d3a9cedf71aee2d9795cb
LAPTOP-LOTTO\botto <luca.bottoli@beije.it>
2022-11-28 17:19:35 +0100

    routing addeed

---
+++ 7emezzo-web/src/screens/Login.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Login from "7emezzo-library/dist/screens/Login"
+
+function LoginScreen(){
+    return(
+        <Login />
+    )
+}
+
+export default LoginScreen  
