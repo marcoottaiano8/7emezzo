57608437cebb10a90ecc02d8d32eb6577a28972e
LAPTOP-LOTTO\botto <luca.bottoli@beije.it>
2022-11-28 17:53:15 +0100

    no message

--- 7emezzo-mobile/App.js
+++ 7emezzo-mobile/App.js
@@ -1,11 +1,46 @@
 import { Login } from '7emezzo-library';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, StatusBar } from 'react-native';
 
 export default function App() {
+
+  let options = {
+    headerStyle: {
+      backgroundColor: 'darkblue',
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+    },
+
+  }
+
   return (
-    <View>
-      <Login />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar></StatusBar>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName={"Login"}>
+          <Stack.Screen
+            name="Login"
+            component={Tutorial}
+            options={options}
+          />
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={options}
+          />
+          <Stack.Screen
+            name="CameraScreen"
+            component={CameraScreen}
+            options={options}
+          />
+          <Stack.Screen
+            name="Gallery"
+            component={Gallery}
+            options={options}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
   );
 }
