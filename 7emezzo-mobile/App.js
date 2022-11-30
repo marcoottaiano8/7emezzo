a9265fb493162a4832931b7c3d15c7b001d4dc8f
marcoottaiano <marco.ottaiano@beije.it>
2022-11-30 10:15:02 +0100

    adding game

--- 7emezzo-mobile/App.js
+++ 7emezzo-mobile/App.js
@@ -4,23 +4,25 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "./view/LoginScreen";
 import SignupScreen from "./view/SignupScreen";
 import HomeScreen from "./view/HomeScreen";
+import GameScreen from "./view/GameScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <StatusBar style="auto" />
-      {/* <SafeAreaView style={{ flex: 1 }}> */}
-      <Stack.Navigator
-        initialRouteName={"Login"}
-        screenOptions={{ headerShown: false }}
-      >
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Signup" component={SignupScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-      {/* </SafeAreaView> */}
+      <StatusBar />
+      <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
+        <Stack.Navigator
+          initialRouteName={"Game"}
+          screenOptions={{ headerShown: false }}
+        >
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Signup" component={SignupScreen} />
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Game" component={GameScreen} />
+        </Stack.Navigator>
+      </SafeAreaView>
     </NavigationContainer>
   );
 }
---
+++ 7emezzo-mobile/view/GameScreen.js
@@ -0,0 +1,6 @@
+import Game from "7emezzo-libraryv2/dist/screens/Game";
+import React from "react";
+
+export default function GameScreen() {
+  return <Game />;
+}
