6085381b1d6dde595516efd6a3f0837faa5685b8
LAPTOP-LOTTO\botto <luca.bottoli@beije.it>
2022-11-30 15:36:02 +0100

    no message

---
+++ 7emezzo-web/src/screens/LoginScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {Login} from "7emezzo-libraryv2/dist/screens/Login"
+
+function LoginScreen (){
+    return(
+        <Login />
+    )
+}
+
+export default LoginScreen
