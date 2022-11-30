436bb9e59f7838b41a4fb73fbf96ee9ffdfdc8f0
marcoottaiano <marco.ottaiano@beije.it>
2022-11-30 16:29:17 +0100

    no message

--- 7emezzo-web/src/index.js
+++ 7emezzo-web/src/index.js
@@ -1,15 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import store from "./redux/store";
-import { Provider } from "react-redux/es/exports";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
