34f836054ebfb98eac32d55e803ad316d030c8fb
LAPTOP-LOTTO\botto <luca.bottoli@beije.it>
2022-11-30 16:33:10 +0100

    no message

--- 7emezzo-web/src/App.js
+++ 7emezzo-web/src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen"; 
 import SignupScreen from "./screens/SignupScreen";
--- 7emezzo-web/src/index.js
+++ 7emezzo-web/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
 import store from "./redux/store";
-import { Provider } from "react-redux/es/exports";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
