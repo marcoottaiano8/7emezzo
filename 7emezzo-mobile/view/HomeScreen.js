21829345c92a38a05deb8af865dc0c0a95b78fcb
marcoottaiano <marco.ottaiano@beije.it>
2022-12-03 17:27:01 +0100

    aggioranmento versione

--- 7emezzo-mobile/view/HomeScreen.js
+++ 7emezzo-mobile/view/HomeScreen.js
@@ -1,12 +1,16 @@
 import Home from "7emezzo-libraryv2/dist/screens/Home";
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
+import { getDataFromStorage } from "7emezzo-libraryv2/dist/utils/utils";
 
 export default function HomeScreen() {
   const navigation = useNavigation();
 
-  function goToRanking() {
+  async function goToRanking() {
+    const token = await getDataFromStorage("token");
+    console.log("token:", token);
     navigation.navigate("Ranking");
   }
+
   return <Home goToRanking={goToRanking} />;
 }
--- 7emezzo-mobile/view/RankingScreen.js
+++ 7emezzo-mobile/view/RankingScreen.js
@@ -1,6 +1,13 @@
 import Ranking from "7emezzo-libraryv2/dist/screens/Ranking";
 import React from "react";
+import { useNavigation } from "@react-navigation/native";
 
 export default function RankingScreen() {
-  return <Ranking />;
+  const navigation = useNavigation();
+
+  function goToHome() {
+    navigation.navigate("Home");
+  }
+
+  return <Ranking goToHome={goToHome} />;
 }
