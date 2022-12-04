817cd1d8f8070c58b492de7764afcb7f3ccf4b01
LAPTOP-LOTTO\botto <luca.bottoli@beije.it>
2022-12-04 16:33:17 +0100

    no message

--- 7emezzo-mobile/view/GameScreen.js
+++ 7emezzo-mobile/view/GameScreen.js
@@ -1,6 +1,14 @@
 import Game from "7emezzo-libraryv2/dist/screens/Game";
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
 
 export default function GameScreen() {
-  return <Game />;
+
+  const navigation = useNavigation();
+
+  function goToHome(){
+    navigation.navigate("Home");
+  }
+
+  return <Game callbackQuit={goToHome} />;
 }
